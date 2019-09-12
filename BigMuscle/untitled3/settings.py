8a9856fbb435b6e1fb5f00b82f6f5491c13f1932
Brady Whildin <bhw29@drexel.edu>
2019-09-12 13:41:57 -0400

    Changed settings.py

--- BigMuscle/untitled3/settings.py
+++ BigMuscle/untitled3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd668vuu8=twa@jh1epp7=&_9wpj7bqdn#!jkv172w8_3#f=$^*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT='staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
