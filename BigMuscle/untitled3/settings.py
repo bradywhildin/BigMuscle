4a2935af16f47b9325ca6e30b0edf6d8e868ab1f
Brady Whildin <bhw29@drexel.edu>
2019-09-13 11:35:08 -0400

    Undid untitled3. before installed apps

--- BigMuscle/untitled3/settings.py
+++ BigMuscle/untitled3/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'untitled3.accounts',
-    'untitled3.fitnesstracker',
-    'untitled3.articles',
+    'accounts',
+    'fitnesstracker',
+    'articles',
 ]
 
 MIDDLEWARE = [
