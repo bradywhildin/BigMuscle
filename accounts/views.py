763b6cf4c509e5b438ca2445abe1066b6abd71ec
Brady Whildin <bhw29@drexel.edu>
2019-09-30 17:46:18 -0400

    Removed unncessary class declarations

--- accounts/views.py
+++ accounts/views.py
@@ -9,28 +9,29 @@ from django.contrib.auth import login, logout
 
 # Handles creation of new user
 def signup_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)  # gives sign up ability to signup.html
-        if form.is_valid():
-            user = form.save()
+    if request.method == 'POST':  # checks if data was sent
+        form = UserCreationForm(request.POST)
+        if form.is_valid():  # checks if username and password meet requirements
+            user = form.save()  # saves user
             login(request, user)  # logs user in after successful sign up
             return redirect('homepage')  # returns user to homepage after successful sign up
     else:
         form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {'form':form})
+    return render(request, 'accounts/signup.html', {'form':form})  # sends user creation form
 
+# Handles login
 def login_view(request):
-    if request.method == 'POST':
+    if request.method == 'POST':  # checks if data was sent
         form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
+        if form.is_valid():  # checks if username and password belongs to existing user
             user = form.get_user()
-            login(request, user)
-            return redirect('homepage')
+            login(request, user)  # logs in user
+            return redirect('homepage')  # redirects to homepage of site
     else:
         form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form':form})
+    return render(request, 'accounts/login.html', {'form':form})  # sends login form
 
 def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect(request.META['HTTP_REFERER'])
+    if request.method == 'POST':  # checks if data was sent
+        logout(request)  # logs out user
+        return redirect(request.META['HTTP_REFERER'])  # returns user to the page they were on
